53f2b7368decde3c8a72d1c8fdd6c7d992810dc5
gvbelavin <97274686+gvbelavin@users.noreply.github.com>
2022-01-07 13:01:12 +0700

    Update boids.py
    Change tabs to spaces

--- boids.py
+++ boids.py
@@ -24,186 +24,186 @@ vp.scene.height = HEIGHT
 
 
 def gravitationalForce(p1, p2):
-	G = 1 # real-world value is : G = 6.67e-11
-	rVector = p1 - p2
-	rMagnitude = vp.mag(rVector)
-	rHat = rVector / rMagnitude
-	F = - rHat * G * BOID_MASS * SPHERE_MASS /rMagnitude**2
-	return F
+    G = 1 # real-world value is : G = 6.67e-11
+    rVector = p1 - p2
+    rMagnitude = vp.mag(rVector)
+    rHat = rVector / rMagnitude
+    F = - rHat * G * BOID_MASS * SPHERE_MASS /rMagnitude**2
+    return F
 
 
 class Boid:
 
-	maxspeed = 2
+    maxspeed = 2
 
-	cohesionRange = 100
-	cohesionStrength = 0.001
+    cohesionRange = 100
+    cohesionStrength = 0.001
 
-	separationRange = 50
-	separationStrength = 0.01
+    separationRange = 50
+    separationStrength = 0.01
 
-	alignementRange = 100
-	alignementStrength = 0.05
+    alignementRange = 100
+    alignementStrength = 0.05
 
-	def __init__(self, position, velocity, radius):
-		self.position = position
-		self.velocity = velocity
-		self.radius = radius
+    def __init__(self, position, velocity, radius):
+        self.position = position
+        self.velocity = velocity
+        self.radius = radius
 
-		r = (random.randint(0, 1) + 1)/2
-		g = (random.randint(0, 1) + 1)/2
-		b = (random.randint(0, 1) + 1)/2
+        r = (random.randint(0, 1) + 1)/2
+        g = (random.randint(0, 1) + 1)/2
+        b = (random.randint(0, 1) + 1)/2
 
-		axis = vp.norm(self.velocity) * 4 * self.radius
+        axis = vp.norm(self.velocity) * 4 * self.radius
 
-		self.figure = vp.cone(
-			pos=self.position,
-			axis=axis,
-			radius=self.radius,
-			color=vp.vector(r, g, b),
-			make_trail=False,
-			trail_type="curve")
+        self.figure = vp.cone(
+            pos=self.position,
+            axis=axis,
+            radius=self.radius,
+            color=vp.vector(r, g, b),
+            make_trail=False,
+            trail_type="curve")
 
-	def update_distances(self):
-		global BOIDS
+    def update_distances(self):
+        global BOIDS
 
-		self.dist = []
-		for obj in BOIDS:
-			d = vp.mag(self.position - obj.position)
-			self.dist.append(d)
+        self.dist = []
+        for obj in BOIDS:
+            d = vp.mag(self.position - obj.position)
+            self.dist.append(d)
 
-	def cohesion(self):
-		global BOIDS
+    def cohesion(self):
+        global BOIDS
 
-		c = vp.vector(0, 0, 0)
-		count = 0
+        c = vp.vector(0, 0, 0)
+        count = 0
 
-		for i, d in enumerate(self.dist):
-			if d > 0 and d < self.cohesionRange:
-				c += BOIDS[i].position
-				count += 1
+        for i, d in enumerate(self.dist):
+            if d > 0 and d < self.cohesionRange:
+                c += BOIDS[i].position
+                count += 1
 
-		if count:
-			return vp.norm(c/count - self.position) * self.cohesionStrength
+        if count:
+            return vp.norm(c/count - self.position) * self.cohesionStrength
 
-		return c
+        return c
 
-	def separation(self):
-		global BOIDS
+    def separation(self):
+        global BOIDS
 
-		c = vp.vector(0, 0, 0)
-		count = 0
+        c = vp.vector(0, 0, 0)
+        count = 0
 
-		for i, d in enumerate(self.dist):
-			if d > 0 and d < self.separationRange:
-				diff = self.position - BOIDS[i].position
-				k = math.pow((self.separationRange - d)/self.separationRange, 2)
-				c += diff * k
-				count += 1
+        for i, d in enumerate(self.dist):
+            if d > 0 and d < self.separationRange:
+                diff = self.position - BOIDS[i].position
+                k = math.pow((self.separationRange - d)/self.separationRange, 2)
+                c += diff * k
+                count += 1
 
-		return vp.norm(c) * self.separationStrength
+        return vp.norm(c) * self.separationStrength
 
-	def alignement(self):
-		global BOIDS
+    def alignement(self):
+        global BOIDS
 
-		c = vp.vector(0, 0, 0)
-		count = 0
+        c = vp.vector(0, 0, 0)
+        count = 0
 
-		for i, d in enumerate(self.dist):
-			if d > 0 and d < self.alignementRange:
-				c += BOIDS[i].velocity
-				count += 1
+        for i, d in enumerate(self.dist):
+            if d > 0 and d < self.alignementRange:
+                c += BOIDS[i].velocity
+                count += 1
 
-		if count:
-			return vp.norm(c - self.velocity) * self.alignementStrength
+        if count:
+            return vp.norm(c - self.velocity) * self.alignementStrength
 
-		return c
+        return c
 
-	def attraction(self):
-		global SPHERES
+    def attraction(self):
+        global SPHERES
 
-		force = vp.vector(0, 0, 0)
-		for obj in SPHERES:
-			force += gravitationalForce(self.position, obj.pos)
-		
-		self.velocity += force * dt/BOID_MASS
-		self.position += self.velocity * t + force * dt**2/BOID_MASS
+        force = vp.vector(0, 0, 0)
+        for obj in SPHERES:
+            force += gravitationalForce(self.position, obj.pos)
+        
+        self.velocity += force * dt/BOID_MASS
+        self.position += self.velocity * t + force * dt**2/BOID_MASS
 
-	def check_borders(self, w, h):
-		changed = False
+    def check_borders(self, w, h):
+        changed = False
 
-		if self.position.x < -w//2:
-			self.position.x += w
-			changed = True
+        if self.position.x < -w//2:
+            self.position.x += w
+            changed = True
 
-		if self.position.x > w//2:
-			self.position.x -= w
-			changed = True
+        if self.position.x > w//2:
+            self.position.x -= w
+            changed = True
 
-		if self.position.y < -h//2:
-			self.position.y += h
-			changed = True
+        if self.position.y < -h//2:
+            self.position.y += h
+            changed = True
 
-		if self.position.y > h//2:
-			self.position.y -= h
-			changed = True
+        if self.position.y > h//2:
+            self.position.y -= h
+            changed = True
 
-		if self.position.z < -h//2:
-			self.position.z += h
-			changed = True
+        if self.position.z < -h//2:
+            self.position.z += h
+            changed = True
 
-		if self.position.z > h//2:
-			self.position.z -= h
-			changed = True
+        if self.position.z > h//2:
+            self.position.z -= h
+            changed = True
 
-		if changed and obj.figure.make_trail:
-			self.figure.clear_trail()
+        if changed and obj.figure.make_trail:
+            self.figure.clear_trail()
 
-	def move(self, w, h):
-		self.update_distances()
+    def move(self, w, h):
+        self.update_distances()
 
-		self.velocity += self.cohesion()
-		self.velocity += self.separation()
-		self.velocity += self.alignement()
-		self.position += self.velocity
+        self.velocity += self.cohesion()
+        self.velocity += self.separation()
+        self.velocity += self.alignement()
+        self.position += self.velocity
 
-		self.attraction()
-		if vp.mag(self.velocity) > Boid.maxspeed:
-			self.velocity = self.velocity/vp.mag(self.velocity) * Boid.maxspeed
+        self.attraction()
+        if vp.mag(self.velocity) > Boid.maxspeed:
+            self.velocity = self.velocity/vp.mag(self.velocity) * Boid.maxspeed
 
-		self.check_borders(w, h)
+        self.check_borders(w, h)
 
-		self.figure.pos = self.position
-		self.figure.axis = vp.norm(self.velocity) * 4 * self.radius
+        self.figure.pos = self.position
+        self.figure.axis = vp.norm(self.velocity) * 4 * self.radius
 
 #==============================================================================
 # Interface handlers
 #==============================================================================
 def createSphere(ev):
-	loc = ev.pos
-	obj = vp.sphere(pos=loc, radius=20, color=vp.color.cyan)
-	SPHERES.append(obj)
+    loc = ev.pos
+    obj = vp.sphere(pos=loc, radius=20, color=vp.color.cyan)
+    SPHERES.append(obj)
 
 
 def show_trail(ev):
-	for obj in BOIDS:
-		obj.figure.clear_trail()
-		obj.figure.make_trail = ev.checked
+    for obj in BOIDS:
+        obj.figure.clear_trail()
+        obj.figure.make_trail = ev.checked
 
 
 def set_cohesionRange(ev):
-	Boid.cohesionRange = ev.value
-	cohesionRangeReadout.text = " {}".format(ev.value)
+    Boid.cohesionRange = ev.value
+    cohesionRangeReadout.text = " {}".format(ev.value)
 
 
 def set_separationRange(ev):
-	Boid.separationRange = ev.value
-	separationRangeReadout.text = " {}".format(ev.value)
+    Boid.separationRange = ev.value
+    separationRangeReadout.text = " {}".format(ev.value)
 
 
 def set_alignementRange(ev):
-	Boid.alignementRange = ev.value
-	alignementRangeReadout.text = " {}".format(ev.value)
+    Boid.alignementRange = ev.value
+    alignementRangeReadout.text = " {}".format(ev.value)
 
 #==============================================================================
 # Interface handlers binding
@@ -212,29 +212,29 @@ vp.scene.bind('click', createSphere)
 
 vp.scene.append_to_caption('\n')
 cohesionRange = vp.slider(bind=set_cohesionRange,
-	vertical=False,
-	min=10, max=150, step=10,
-	value=100,
-	length=200, width=10)
+    vertical=False,
+    min=10, max=150, step=10,
+    value=100,
+    length=200, width=10)
 vp.scene.append_to_caption("Cohesion range =")
 cohesionRangeReadout = vp.wtext(text=" {}".format(Boid.cohesionRange))
 
 
 vp.scene.append_to_caption('\n')
 separationRange = vp.slider(bind=set_separationRange,
-	vertical=False,
-	min=10, max=150, step=10,
-	value=50,
-	length=200, width=10)
+    vertical=False,
+    min=10, max=150, step=10,
+    value=50,
+    length=200, width=10)
 vp.scene.append_to_caption("Separation range =")
 separationRangeReadout = vp.wtext(text=" {}".format(Boid.separationRange))
 
 vp.scene.append_to_caption('\n')
 alignementRange = vp.slider(bind=set_alignementRange,
-	vertical=False,
-	min=10, max=150, step=10,
-	value=100,
-	length=200, width=10)
+    vertical=False,
+    min=10, max=150, step=10,
+    value=100,
+    length=200, width=10)
 vp.scene.append_to_caption("Alignement range =")
 alignementRangeReadout = vp.wtext(text=" {}".format(Boid.alignementRange))
 
@@ -245,22 +245,22 @@ vp.checkbox(bind=show_trail, text="Show trail")
 # Bounding box
 #==============================================================================
 mybox = vp.box(pos=vp.vector(0, 0, 0),
-	length=WIDTH, height=WIDTH, width=HEIGHT, opacity=0.1) 
+    length=WIDTH, height=WIDTH, width=HEIGHT, opacity=0.1) 
 
 #==============================================================================
 # Generate boids
 #==============================================================================
 for i in range(NBOIDS):
-	x = random.randint(-WIDTH//2, WIDTH//2)
-	y = random.randint(-HEIGHT//2, HEIGHT//2)
-	z = random.randint(-HEIGHT//2, HEIGHT//2)
+    x = random.randint(-WIDTH//2, WIDTH//2)
+    y = random.randint(-HEIGHT//2, HEIGHT//2)
+    z = random.randint(-HEIGHT//2, HEIGHT//2)
 
-	vx = random.randint(-2, 2)
-	vy = random.randint(-2, 2)
-	vz = random.randint(-2, 2)
+    vx = random.randint(-2, 2)
+    vy = random.randint(-2, 2)
+    vz = random.randint(-2, 2)
 
-	obj = Boid(vp.vector(x, y, z), vp.vector(vx, vy, vz), BOID_SIZE)
-	BOIDS.append(obj)
+    obj = Boid(vp.vector(x, y, z), vp.vector(vx, vy, vz), BOID_SIZE)
+    BOIDS.append(obj)
 
 #==============================================================================
 # Main cycle
@@ -269,7 +269,7 @@ t = 0
 dt = 0.001 #The step size. This should be a small number
 
 while True:
-	vp.rate(1000)
-	for obj in BOIDS:
-		obj.move(WIDTH, HEIGHT)
-	t += dt
+    vp.rate(1000)
+    for obj in BOIDS:
+        obj.move(WIDTH, HEIGHT)
+    t += dt
